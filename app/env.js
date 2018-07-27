67bf4ac92c068d19bcc1da1025eee75dcc11edb7
Stephen Andrews <stephen@andrewstribe.co.nz>
2018-07-28 04:35:56 +1200

    Master env file

--- app/env.js
+++ app/env.js
@@ -1,12 +1,11 @@
-var API_URL = 'http://45.79.105.117:8338/api',
-EXPLORER_URL = 'http://zeronet.tzscan.io/',
-ISDEV = true,
+var API_URL = 'http://45.56.84.80:8338/api',
+EXPLORER_URL = 'http://tzscan.io/',
 CONSTANTS = {
-  cycle_length : 128,
+  cycle_length : 4096,
   commitment : 32,
-  block_time : 20,
+  block_time : 60,
 };
 
 //Set node
-eztz.node.setProvider('http://45.56.90.73:3000');
-eztz.node.setDebugMode(true);
+eztz.node.setProvider('https://rpc.tezrpc.me');
+eztz.node.setDebugMode(false);
