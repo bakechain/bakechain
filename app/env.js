fc521bc6ad658d704015c75a075c163f863e4f93
Stephen Andrews <stephen@andrewstribe.co.nz>
2018-07-30 16:14:45 +1200

    retain env for dev

--- app/env.js
+++ app/env.js
@@ -1,11 +1,15 @@
-var API_URL = 'http://45.56.84.80:8338/api',
-EXPLORER_URL = 'http://tzscan.io/',
+//DEV ENV
+
+var API_URL = 'http://45.79.105.117:8338/api',
+EXPLORER_URL = 'http://zeronet.tzscan.io/',
 DEBUGMODE = true,
 CONSTANTS = {
-  cycle_length : 4096,
+  cycle_length : 128,
   commitment : 32,
-  block_time : 60,
+  block_time : 20,
   threshold : 70368744177663,
 };
-eztz.node.setProvider('https://rpc.tezrpc.me');
-eztz.node.setDebugMode(false);
+
+//Set node
+//eztz.node.setDebugMode(true);
+eztz.node.setProvider('http://45.56.90.73:3000');
