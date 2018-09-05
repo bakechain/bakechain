b041814e72c671b7faa812c8790484e202b7fe9e
Stephen Andrews <stephen@andrewstribe.co.nz>
2018-09-06 00:48:40 +1200

    7.0.0 update

--- app/env.js
+++ app/env.js
@@ -1,10 +1,10 @@
-var VERSION = '6.0.0',
+var VERSION = '7.0.0',
 API_URL = 'http://96.126.99.79:8338/api',
 EXPLORER_URL = 'http://tzscan.io/',
 NODE_ADDRESS = 'https://rpc.tezrpc.me',
 DEBUGMODE = false,
 CONSTANTS = {
-  mempool : 'mempool/pending_operations',
+  mempool : 'mempool',
   cycle_length : 4096,
   commitment : 32,
   block_time : 60,
