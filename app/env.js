dc422d83ab719c595912b879679ad7252e297e17
Stephen Andrews <stephen@andrewstribe.co.nz>
2018-09-06 01:23:57 +1200

    dev env

--- app/env.js
+++ app/env.js
@@ -1,13 +1,16 @@
-var VERSION = '6.0.0',
-API_URL = 'http://96.126.99.79:8338/api',
-EXPLORER_URL = 'http://tzscan.io/',
-NODE_ADDRESS = 'https://rpc.tezrpc.me',
-DEBUGMODE = false,
+var VERSION = 'zero7.0.0',
+API_URL = 'http://45.79.105.117:8338/api',
+EXPLORER_URL = 'http://zeronet.tzscan.io/',
+NODE_ADDRESS = 'https://zeronet.simplestaking.com:3000',
+DEBUGMODE = true,
 CONSTANTS = {
   mempool : 'mempool/pending_operations',
-  cycle_length : 4096,
+  cycle_length : 128,
   commitment : 32,
-  block_time : 60,
+  block_time : 20,
   threshold : 70368744177663,
 };
-eztz.node.setDebugMode(false);
+
+//Set node
+//eztz.node.setDebugMode(true);
+eztz.node.setProvider('https://zeronet.simplestaking.com:3000');
